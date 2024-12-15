09f9097d846fe602084a5fd0eee5a66e6e61896f
Bernd Schoeller <bernd@fams.de>
2024-12-15 06:17:46 +0000

    Day 15 part 1 and 2

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,11 +1,9 @@
 use adventofcode2024::ascii::{Dir, Map, Pos};
 use adventofcode2024::input::AocInput;
-use regex::Regex;
-use std::collections::HashSet;
 use std::time::Instant;
 
-fn gps(map: &Map) -> i64 {
-    map.find('O').iter().map(|p| p.0 + 100 * p.1).sum()
+fn gps(map: &Map, c: char) -> i64 {
+    map.find(c).iter().map(|p| p.0 + 100 * p.1).sum()
 }
 
 fn try_step(m: &mut Map, p: Pos, d: Dir) -> bool {
@@ -25,23 +23,145 @@ fn try_step(m: &mut Map, p: Pos, d: Dir) -> bool {
     false
 }
 
+fn can_step(m: &Map, p: Pos, d: Dir) -> bool {
+    let i = m.get(p);
+    let t = p.step(d);
+    match i {
+        '.' => true,
+        '#' => false,
+        '[' => {
+            if d == Dir::N || d == Dir::S {
+                let r = t.step(Dir::E);
+                can_step(m, t, d) && can_step(m, r, d)
+            } else {
+                can_step(m, t.step(d), d)
+            }
+        }
+        ']' => {
+            if d == Dir::N || d == Dir::S {
+                let r = t.step(Dir::W);
+                can_step(m, t, d) && can_step(m, r, d)
+            } else {
+                can_step(m, t.step(d), d)
+            }
+        }
+        _ => can_step(m, t, d)
+    }
+}
+
+fn do_step(m: &mut Map, p: Pos, d: Dir) {
+    let i = m.get(p);
+    let t = p.step(d);
+    match i {
+        '.' => {}
+        '#' => panic!("Trying to move walls"),
+        '[' => {
+            match d {
+                Dir::N | Dir::S => {
+                    do_step(m, t, d);
+                    do_step(m, t.step(Dir::E), d);
+                    m.set(t, '[');
+                    m.set(t.step(Dir::E), ']');
+                    m.set(p, '.');
+                    m.set(p.step(Dir::E), '.');
+                }
+                Dir::E => {
+                    do_step(m, t.step(d), d);
+                    m.set(p.step(d).step(d), ']');
+                    m.set(p.step(d), '[');
+                    m.set(p, '.');
+                }
+                _ => panic!("Illegal move")
+            }
+        }
+        ']' => {
+            match d {
+                Dir::N | Dir::S => {
+                    do_step(m, t, d);
+                    do_step(m, t.step(Dir::W), d);
+                    m.set(t, ']');
+                    m.set(t.step(Dir::W), '[');
+                    m.set(p, '.');
+                    m.set(p.step(Dir::W), '.');
+                }
+                Dir::W => {
+                    do_step(m, t.step(d), d);
+                    m.set(p.step(d).step(d), '[');
+                    m.set(p.step(d), ']');
+                    m.set(p, '.');
+                }
+                _ => panic!("Illegal move")
+            }
+        }
+        _ => {
+            do_step(m, t, d);
+            m.set(t, '@');
+            m.set(p, '.');
+        }
+    }
+}
+
+fn stretch(map: &Map) -> Map {
+    let mut res = Map::new(map.width * 2, map.height, '?');
+    for (p, c) in map.enumerate() {
+        let p1 = Pos(p.0 * 2, p.1);
+        let p2 = Pos(p.0 * 2 + 1, p.1);
+        match c {
+            '.' => {
+                res.set(p1, '.');
+                res.set(p2, '.');
+            }
+            '#' => {
+                res.set(p1, '#');
+                res.set(p2, '#');
+            }
+            'O' => {
+                res.set(p1, '[');
+                res.set(p2, ']');
+            }
+            '@' => {
+                res.set(p1, '@');
+                res.set(p2, '.');
+            }
+            _ => panic!("Unknown map content!")
+        }
+    }
+    res
+}
+
 fn part1(input: &mut AocInput) {
     let mut state = input.read_map();
-    let dirs: Vec<Dir> = input.read_lines().iter().map(|l| l.chars().map(|c| Dir::from(c))).flatten().collect();
+    let dirs: Vec<Dir> = input
+        .read_lines()
+        .iter()
+        .map(|l| l.chars().map(|c| Dir::from(c)))
+        .flatten()
+        .collect();
     let mut robot = *state.find('@').first().unwrap();
-    println!("{}", state.to_string());
     for d in dirs {
         if try_step(&mut state, robot, d) {
             robot = robot.step(d);
         }
-        println!("{}", state.to_string());
     }
-
-    println!("Part 1: {:?}", gps(&state));
+    println!("Part 1: {:?}", gps(&state, 'O'));
 }
 
-fn part2() {
-    println!("Part 2: {:?}", 2);
+fn part2(input: &mut AocInput) {
+    let mut state = stretch(&input.read_map());
+    let dirs: Vec<Dir> = input
+        .read_lines()
+        .iter()
+        .map(|l| l.chars().map(|c| Dir::from(c)))
+        .flatten()
+        .collect();
+    let mut robot = *state.find('@').first().unwrap();
+    for d in dirs {
+        if can_step(&state, robot, d) {
+            do_step(&mut state, robot, d);
+            robot = robot.step(d);
+        }
+    }
+    println!("Part 2: {}", gps(&state, '['));
 }
 
 fn main() {
@@ -49,7 +169,8 @@ fn main() {
     let mut input = AocInput::new("inputs/day15.txt");
 
     part1(&mut input);
-    part2();
+    input.reset();
+    part2(&mut input);
 
     let duration = start.elapsed();
     println!("Time: {:?}", duration);

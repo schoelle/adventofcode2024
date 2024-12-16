912ecf7cd376e0de1da069aa783365d5a76b2e1e
Bernd Schoeller <bernd@fams.de>
2024-12-16 22:29:52 +0000

    Day 16 part 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,11 +1,11 @@
 use adventofcode2024::ascii::{Dir, Map, Pos};
 use adventofcode2024::input::AocInput;
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::time::Instant;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct State(Pos, Dir, i64);
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct State(Vec<Pos>, Dir, i64);
 
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -22,29 +22,65 @@ impl PartialOrd for State {
 fn part1(map: &Map, start: Pos, end: Pos) {
     let mut work: BinaryHeap<State> = BinaryHeap::new();
     let mut done: HashSet<(Pos, Dir)> = HashSet::new();
-    work.push(State(start, Dir::E, 0));
+    work.push(State(vec![start], Dir::E, 0));
     while let Some(s) = work.pop() {
-        if done.contains(&(s.0, s.1)) {
+        let pos = *s.0.last().unwrap();
+        let dir = s.1;
+        if done.contains(&(pos, dir)) {
             continue;
         }
-        if s.0 == end {
+        done.insert((pos, dir));
+        if pos == end {
             println!("Part 1: {:?}", s.2);
             return;
         }
-        work.push(State(s.0, s.1.right90(), s.2 + 1000));
-        work.push(State(s.0, s.1.left90(), s.2 + 1000));
-        let t = s.0.step(s.1);
-        if map.get(t) == '.' {
-            work.push(State(t, s.1, s.2 + 1));
+        work.push(State(s.0.clone(), dir.right90(), s.2 + 1000));
+        work.push(State(s.0.clone(), dir.left90(), s.2 + 1000));
+        let target = pos.step(dir);
+        if map.get(target) == '.' {
+            let mut extended_path = s.0.clone();
+            extended_path.push(target);
+            work.push(State(extended_path, dir, s.2 + 1));
         }
-        done.insert((s.0, s.1));
     }
     panic!("No path found!");
 }
 
-fn part2(_map: &Map, start: Pos, end: Pos) {
-    println!("Part 2: {}", 2);
+
+fn part2(map: &Map, start: Pos, end: Pos) {
+    let mut work: BinaryHeap<State> = BinaryHeap::new();
+    let mut done: HashMap<(Pos, Dir), i64> = HashMap::new();
+    let mut best: Vec<State> = Vec::new();
+    work.push(State(vec![start], Dir::E, 0));
+    while let Some(s) = work.pop() {
+        let pos = *s.0.last().unwrap();
+        let dir = s.1;
+        if let Some(cc) = done.get(&(pos, dir)) {
+            if *cc < s.2 {
+                continue;
+            }
+        }
+        done.insert((pos, dir), s.2);
+        if pos == end {
+            if !best.is_empty() && best.first().unwrap().2 < s.2 {
+                break;
+            }
+            best.push(s.clone());
+        }
+        work.push(State(s.0.clone(), dir.right90(), s.2 + 1000));
+        work.push(State(s.0.clone(), dir.left90(), s.2 + 1000));
+        let target = pos.step(dir);
+        if map.get(target) == '.' {
+            let mut extended_path = s.0.clone();
+            extended_path.push(target);
+            work.push(State(extended_path, dir, s.2 + 1));
+        }
+    }
+
+    let cnt: HashSet<Pos> = best.iter().map(|s| s.0.clone()).flatten().collect();
+    println!("Part 2: {:?}", cnt.len());
 }
+
 
 fn main() {
     let start = Instant::now();

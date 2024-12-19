9febf32bb62f56bacbee38ecebd0ac4927e69d87
Bernd Schoeller <bernd@fams.de>
2024-12-19 19:07:34 +0000

    Day 19 part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,19 +1,48 @@
 use adventofcode2024::input::AocInput;
-use std::time::Instant;
 use regex::Regex;
-
+use std::collections::HashMap;
+use std::time::Instant;
 
 fn part1(towel_str: &str, patterns: &Vec<String>) {
-    let mut exp:String = "^(".to_string();
+    let mut exp: String = "^(".to_string();
     exp.push_str(&towel_str.replace(", ", "|"));
     exp.push_str(")+$");
-    let re = Regex::new(&exp).unwrap();;
+    let re = Regex::new(&exp).unwrap();
 
-    println!("Part 1: {}", patterns.iter().filter(|p| re.is_match(p)).count())
+    println!(
+        "Part 1: {}",
+        patterns.iter().filter(|p| re.is_match(p)).count()
+    )
 }
 
-fn part2() {
-    println!("Part 2: {}", 2);
+fn matches(towels: &Vec<&str>, pattern: &str, cache: &mut HashMap<String, i64>) -> i64 {
+    if let Some(x) = cache.get(pattern) {
+        return *x;
+    }
+    let mut r = 0;
+    for p in towels {
+        if pattern.starts_with(p) {
+            let l = p.len();
+            if l == pattern.len() {
+                r += 1;
+            } else {
+                r += matches(towels, &pattern[l..], cache);
+            }
+        }
+    }
+    cache.insert(pattern.to_string(), r);
+    r
+}
+
+fn part2(towels: &Vec<&str>, patterns: &Vec<String>) {
+    let mut cache: HashMap<String, i64> = HashMap::new();
+    println!(
+        "Part 2: {}",
+        patterns
+            .iter()
+            .map(|p| matches(towels, p, &mut cache))
+            .sum::<i64>()
+    );
 }
 
 fn main() {
@@ -24,6 +53,7 @@ fn main() {
     let patterns = input.read_lines();
 
     part1(towel_str, &patterns);
+    part2(&towel_str.split(", ").collect(), &patterns);
 
     let duration = start.elapsed();
     println!("Time: {:?}", duration);

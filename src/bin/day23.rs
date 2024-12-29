cdb62bdfed2d7040446fd4be32664a5db0f43588
Bernd Schoeller <bernd@fams.de>
2024-12-29 18:46:29 +0000

    Day 23 part 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,39 +1,45 @@
 use adventofcode2024::input::AocInput;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
-fn main() {
-    let start = Instant::now();
-    let mut input = AocInput::new("inputs/day23.txt");
+type Nodes = HashSet<String>;
+type Edges = HashMap<String, Nodes>;
 
-    let mut graph: HashSet<(String, String)> = HashSet::new();
-    let mut nodes: HashSet<String> = HashSet::new();
-
-    let re = Regex::new("(..)-(..)").unwrap();
-    for line in input.read_lines() {
-        if let Some(m) = re.captures(&line) {
-            let first = m.get(1).unwrap().as_str().to_string();
-            let second = m.get(2).unwrap().as_str().to_string();
-            graph.insert((first.clone(), second.clone()));
-            graph.insert((second.clone(), first.clone()));
-            nodes.insert(first);
-            nodes.insert(second);
-        }
+fn bron_kerbosch(graph: &Edges, r: Nodes, mut p: Nodes, mut x: Nodes, cliques: &mut Vec<Nodes>) {
+    if p.is_empty() && x.is_empty() {
+        cliques.push(r);
+    } else if !p.is_empty() {
+        let nodes = p.iter().cloned().collect::<Nodes>();
+        nodes.iter().for_each(|node| {
+            let neighbours: &Nodes = graph.get(node).unwrap();
+            let mut to_add: Nodes = Nodes::new();
+            to_add.insert(node.clone());
+            bron_kerbosch(
+                graph,
+                r.union(&to_add).cloned().collect(),
+                p.intersection(&neighbours).cloned().collect(),
+                x.intersection(&neighbours).cloned().collect(),
+                cliques,
+            );
+            p.remove(node);
+            x.insert(node.clone());
+        });
     }
+}
 
-    let mut cnt = 0;
-    for a in &nodes {
-        for b in &nodes {
+fn part1(graph: &Edges, nodes: &Nodes) {
+    let mut cnt = 0i64;
+    for a in nodes {
+        for b in nodes {
             if b > a {
-                for c in &nodes {
+                for c in nodes {
                     if c > b
-                        && graph.contains(&(a.clone(), b.clone()))
-                        && graph.contains(&(b.clone(), c.clone()))
-                        && graph.contains(&(c.clone(), a.clone()))
+                        && graph.get(a).unwrap().contains(b)
+                        && graph.get(b).unwrap().contains(c)
+                        && graph.get(c).unwrap().contains(a)
                     {
                         if a.starts_with("t") || b.starts_with("t") || c.starts_with("t") {
-                            println!("{} {} {}", a, b, c);
                             cnt += 1;
                         }
                     }
@@ -42,6 +48,54 @@ fn main() {
         }
     }
     println!("Part 1: {}", cnt);
+}
+
+fn part2(graph: &Edges, nodes: &Nodes) {
+    let mut cliques = Vec::new();
+    bron_kerbosch(
+        &graph,
+        Nodes::new(),
+        nodes.clone(),
+        Nodes::new(),
+        &mut cliques,
+    );
+    let mut largest: Vec<String> = Vec::new();
+    for c in cliques {
+        if c.len() > largest.len() {
+            largest = c.into_iter().collect();
+        }
+    }
+    largest.sort();
+    println!("Part 2: {}", &largest.join(","));
+}
+
+fn main() {
+    let start = Instant::now();
+    let mut input = AocInput::new("inputs/day23.txt");
+
+    let mut nodes: Nodes = Nodes::new();
+    let mut graph: Edges = Edges::new();
+
+    let re = Regex::new("(..)-(..)").unwrap();
+    for line in input.read_lines() {
+        if let Some(m) = re.captures(&line) {
+            let first = m.get(1).unwrap().as_str().to_string();
+            let second = m.get(2).unwrap().as_str().to_string();
+            graph
+                .entry(first.clone())
+                .or_insert(Nodes::new())
+                .insert(second.clone());
+            graph
+                .entry(second.clone())
+                .or_insert(Nodes::new())
+                .insert(first.clone());
+            nodes.insert(first);
+            nodes.insert(second);
+        }
+    }
+
+    part1(&graph, &nodes);
+    part2(&graph, &nodes);
 
     let duration = start.elapsed();
     println!("Time: {:?}", duration);
